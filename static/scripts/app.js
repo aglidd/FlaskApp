2e06d62952ddb33e71b25d3fb862de0118e773d6
CoupB0y <aglidden9@gmail.com>
2021-12-20 23:28:18 -0600

    editing colors of particles

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -29,7 +29,7 @@ particlesJS('particles-js',
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#810000"
+          "color": "#000000"
         },
         "polygon": {
           "nb_sides": 5
