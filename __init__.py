9e22b3393770fca9d24ad1de1859a594cd3e84e0
CoupB0y <aglidden9@gmail.com>
2021-12-21 23:41:29 -0600

    added about and resume pages

--- __init__.py
+++ __init__.py
@@ -7,5 +7,14 @@ def homepage():
     return render_template("main.html")
 
 
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run()
---
+++ static/scripts/main.js
@@ -0,0 +1,6 @@
+function showResume()
+{
+	document.getElementById('home').style.display="none";
+	document.getElementById('resume').style.display="block";
+
+}
