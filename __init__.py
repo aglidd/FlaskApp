c80cd9596f52ff40ed0132f8a4eca618c5ee7dd3
amglidden <amglidden@crimson.ua.edu>
2022-10-05 15:07:29 -0500

    removed file

--- __init__.py
+++ __init__.py
@@ -10,9 +10,6 @@ def homepage():
 def about():
     return render_template("about.html")
 
-@app.route('/peyton')
-def peyton():
-    return render_template("peyton.html")
 
 if __name__ == "__main__":
     app.run()
