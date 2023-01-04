d42bc0942e289b3a4fddb46b9586a5c6e380a594
amglidden <amglidden@crimson.ua.edu>
2023-01-03 21:15:34 -0600

    formatting

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
     return render_template("main.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template("about.html")
 
